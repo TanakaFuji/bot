a26e68ecf370d41e3260ea5dc3086fef03728ea6
TanakaFuji <tanakakai570@gmail.com>
2022-10-28 02:20:00 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -493,7 +493,7 @@ const listMessage = {
                 {
                     urlButton: {
                         displayText: `SUBSCRIBE`,
-                        url: 'https://youtubr.com/c/LeonGanz'
+                        url: 'https://youtubr.com/c/DUCK_Tys'
                     }
                 },
                 {
